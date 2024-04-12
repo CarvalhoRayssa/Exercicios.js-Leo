ab5e18fda238a27ebacdf5b23b2beeb5fb1d8c13
Rayssa <112430158+CarvalhoRayssa@users.noreply.github.com>
2024-04-12 10:08:47 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,22 @@
+// exercicio 1
+
+//const arrayNum = [1, 2, 3];
+//const copiadoNumeros = [...arrayNum] ;
+
+//console.log('La cópia del originale', copiadoNumeros)
+
+
+// exercicio 2
+
+//const isMaiorDeIdade = 20;
+//const mensager = isMaiorDeIdade >= 18? 'Idade das biritas' : 'Menor da quebrada';
+
+//console.log(mensager);
+
+//exercicio 3
+
+const arrayI = [1,2,3];
+const arrayF = [8,9,10];
+const concatenarArrays = [...arrayI, ...arrayF];
+
+console.log('arrays concatenados', concatenarArrays);
